60a756ada9cf06a5044d9639ca4c8897db3517fd
Thomas Lemarchand <tlemarchand@users.noreply.github.com>
2019-08-07 16:52:09 +0200

    Simplify build

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-FROM nginx
+FROM nginx:1.17
 
 ARG version
 
-COPY riot-${version} /usr/share/nginx/html
+RUN apt-get update && apt-get install -y curl && \
+    curl -SL https://github.com/vector-im/riot-web/releases/download/${version}/riot-${version}.tar.gz | tar xzC /usr/share/nginx/html/ && \
+    apt-get remove --purge -y curl && \
+    apt-get autoremove --purge -y && \
+    rm -rf /var/lib/apt/lists/*
--- build.sh
+++ build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 riot_latest=`curl -s 'https://api.github.com/repos/vector-im/riot-web/releases/latest' | jq -r '.name'`
-builded=`curl --write-out %{http_code} --silent --output /dev/null https://registry.hub.docker.com/v2/repositories/tlemarchand/riot-web/tags/$riot_latest/`
 
-if [[ $builded == '200' ]]; then
-  exit 0
-fi
-
-dl_url=`curl -s 'https://api.github.com/repos/vector-im/riot-web/releases/latest' | jq -r '.assets[0].browser_download_url'`
-
-wget $dl_url -O $riot_latest.tar.gz
-tar xvzf $riot_latest.tar.gz
 docker build -t tlemarchand/riot-web:$riot_latest --build-arg version=$riot_latest .
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USER}" --password-stdin
